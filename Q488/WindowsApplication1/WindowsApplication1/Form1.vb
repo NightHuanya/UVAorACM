bc9c33ba03f78153f330b34c3d74fa0583252012
NightHuanya <47856051+NightHuanya@users.noreply.github.com>
2022-12-07 19:06:23 +0800

    Add files via upload

---
+++ Q488/WindowsApplication1/WindowsApplication1/Form1.vb
@@ -0,0 +1,58 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Dim a, c, d As Integer
+        Dim b$
+
+        FileOpen(1, "in1.txt", 1)
+        FileOpen(2, "out1.txt", 2)
+
+        a = LineInput(1)
+
+        For i = 1 To a
+
+            b = LineInput(1)
+
+            d = LineInput(1)
+
+            c = LineInput(1)
+
+            For j = 1 To c
+
+                For k = 1 To d
+
+                    For l = 1 To k
+                        If l = k Then
+                            PrintLine(2, k)
+                        Else
+                            Print(2, k)
+                        End If
+                        
+                    Next
+
+                    If k = d Then
+                        For m = k - 1 To 1 Step -1
+                            For n = m To 1 Step -1
+                                If n = 1 Then
+                                    PrintLine(2, m)
+                                Else
+                                    Print(2, m)
+                                End If
+
+
+                            Next
+
+                        Next
+                    End If
+
+
+
+                Next
+                If j <> c Then
+                    Print(2, vbNewLine)
+                End If
+            Next
+
+        Next
+    End Sub
+End Class
---
+++ Q494/Q494/Q494/Q494/Form1.vb
@@ -0,0 +1,27 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        FileOpen(1, "in1.txt", 1)
+        FileOpen(2, "out1.txt", 2)
+        Dim a, c As String
+        Dim b As Single
+
+        Do Until EOF(1) = True
+            b = 0
+            a = LineInput(1)
+
+            For i = 1 To Len(a)
+                c = Mid(a, i, 1)
+                If Asc(c) < 65 Or Asc(c) > 90 And Asc(c) < 97 Or Asc(c) > 122 Then
+                    Select Case Mid(a, i - 1, 1)
+                        Case "A" To "Z", "a" To "z"
+                            b = b + 1
+                    End Select
+                End If
+            Next
+            PrintLine(2, b)
+        Loop
+    End Sub
+
+    
+End Class
