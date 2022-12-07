d86767181d85b95f04d911b6e711b153d1398548
NightHuanya <47856051+NightHuanya@users.noreply.github.com>
2022-12-07 19:47:16 +0800

    Add files via upload

---
+++ Q12195/Q12195/Form1.vb
@@ -0,0 +1,57 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        FileOpen(1, "in1.txt", 1)
+        FileOpen(2, "out1.txt", 2)
+
+        Dim W, H, Q, F, S, T, X As Single
+        Dim a, b As Single
+        Dim c$
+
+        W = 1 : H = 1 / 2 : Q = 1 / 4 : F = 1 / 8 : S = 1 / 16 : T = 1 / 32 : X = 1 / 64
+
+        c = LineInput(1)
+
+        Do Until c = "*"
+
+            For i = 1 To Len(c)
+                Select Case Mid(c, i, 1)
+                    Case "W"
+                        a = a + W
+                    Case "H"
+                        a = a + H
+                    Case "Q"
+                        a = a + Q
+                    Case "E"
+                        a = a + F
+                    Case "S"
+                        a = a + S
+                    Case "T"
+                        a = a + T
+                    Case "X"
+                        a = a + X
+                    Case Else
+                        If a = 1 Then
+                            b = b + 1
+                        End If
+                        a = 0
+                       
+                End Select
+
+            Next
+
+            c = LineInput(1)
+
+            If c <> "*" Then
+                PrintLine(2, b)
+            Else
+                Print(2, b)
+            End If
+            b = 0
+        Loop
+
+
+
+
+    End Sub
+End Class
---
+++ Q12289/Q12289/Form1.vb
@@ -0,0 +1,54 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        FileOpen(1, "in1.txt", 1)
+        FileOpen(2, "out1.txt", 2)
+
+        Dim a, b As Integer
+        Dim d As String
+        Dim c(0, 2)
+        For z = 0 To 2
+            c(0, z) = Mid("one", z + 1, 1)
+        Next
+
+        a = LineInput(1)
+
+        For i = 1 To a
+
+            d = LineInput(1)
+
+            If Len(d) = 5 Then
+                If i = a Then
+                    Print(2, 3)
+                Else
+                    PrintLine(2, 3)
+                End If
+            Else
+                For j = 1 To Len(d)
+                    If Mid(d, j, 1) = c(0, j - 1) Then
+                        b = b + 1
+                    End If
+                Next
+                If b >= 2 Then
+                    If i = a Then
+                        Print(2, 1)
+                    Else
+                        PrintLine(2, 1)
+                    End If
+                Else
+                    If i = a Then
+                        Print(2, 2)
+                    Else
+                        PrintLine(2, 2)
+                    End If
+                End If
+            End If
+            b = 0
+        Next
+
+
+
+
+
+    End Sub
+End Class
