a61f7e70f06413fb8b3b07237b5b78c145cbd8c3
NightHuanya <47856051+NightHuanya@users.noreply.github.com>
2022-12-08 02:38:30 +0800

    Add files via upload

---
+++ Q13187/Q13187/Form1.vb
@@ -0,0 +1,35 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        FileOpen(1, "in1.txt", 1)
+        FileOpen(2, "out1.txt", 2)
+
+        Dim a, b, c, d As String
+        c = 1
+        a = LineInput(1)
+        d = 0.0
+        For i = 1 To Val(a)
+            b = LineInput(1)
+
+            Do
+                d = Val(c + 1) ^ 0.5 - 1
+
+                If d = b Then
+                    Exit Do
+                Else
+                    c = Val(c) + 1
+
+                End If
+
+            Loop
+
+            If i = Val(a) Then
+                Print(2, c)
+            Else
+                PrintLine(2, c)
+            End If
+            c = 1
+        Next
+
+    End Sub
+End Class
---
+++ Q13216/Q13216/Form1.vb
@@ -0,0 +1,51 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        FileOpen(1, "in1.txt", 1)
+        FileOpen(2, "out1.txt", 2)
+
+        Dim a, b, c As String
+
+        a = LineInput(1)
+
+        For i = 1 To Val(a)
+
+            b = LineInput(1)
+
+            c = Val(b)
+            If Val(c) > 3 Then
+                c = Val(c) Mod 4
+                Select Case Val(c)
+                    Case 0
+                        c = 36
+                    Case 1
+                        c = 16
+                    Case 2
+                        c = 56
+                    Case 3
+                        c = 96
+
+                End Select
+            Else
+                Select Case Val(c)
+                    Case 0
+                        c = 1
+                    Case 1
+                        c = 66
+                    Case 2
+                        c = 56
+                    Case 3
+                        c = 96
+                End Select
+            End If
+            If i = Val(a) Then
+                Print(2, c)
+            Else
+                PrintLine(2, c)
+            End If
+            c = ""
+        Next
+
+
+    End Sub
+End Class
