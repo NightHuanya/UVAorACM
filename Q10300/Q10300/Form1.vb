a48383348552178b12332bc47a40abe4f3c28279
NightHuanya <47856051+NightHuanya@users.noreply.github.com>
2022-12-07 18:54:31 +0800

    Add files via upload

---
+++ Q10300/Q10300/Form1.vb
@@ -0,0 +1,50 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Dim a, b, c, f, g, h, l As Single
+        Dim d$
+        c = 1
+
+        FileOpen(1, "in1.txt", 1)
+        FileOpen(2, "out1.txt", 2)
+
+        a = LineInput(1)
+
+        For i = 1 To a
+            b = LineInput(1)
+            For j = 1 To b
+                d = LineInput(1)
+                For k = 1 To Len(d)
+                    Select Case Mid(d, k, 1)
+                        Case "0" To "9"
+                            If c = 1 Then
+                                f = f & Mid(d, k, 1)
+                            ElseIf c = 2 Then
+                                g = g & Mid(d, k, 1)
+                            ElseIf c = 3 Then
+                                h = h & Mid(d, k, 1)
+                            End If
+
+                        Case Else
+                            c = c + 1
+                    End Select
+                Next
+                l = l + (f / g) * h * g
+
+                f = 0
+                g = 0
+                h = 0
+                c = 1
+            Next
+            If i = a Then
+                Print(2, l)
+            Else
+                PrintLine(2, l)
+            End If
+            l = 0
+        Next
+
+
+
+    End Sub
+End Class
