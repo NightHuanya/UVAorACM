c93afd746ec0df3474f611d9ccee2f9b8bd54121
NightHuanya <47856051+NightHuanya@users.noreply.github.com>
2022-12-07 23:52:39 +0800

    Add files via upload

---
+++ Q11946/Q11946/Form1.vb
@@ -0,0 +1,57 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+        FileOpen(1, "in1.txt", 1)
+        FileOpen(2, "out1.txt", 2)
+
+        Dim a, b As Integer
+        Dim c, d As String
+
+        a = LineInput(1)
+
+        For i = 1 To a
+            c = LineInput(1)
+            Do
+
+                For j = 1 To Len(c)
+                    Select Case Mid(c, j, 1)
+                        Case "0"
+                            d = d & "O"
+                        Case "1"
+                            d = d & "I"
+                        Case "2"
+                            d = d & "Z"
+                        Case "3"
+                            d = d & "E"
+                        Case "4"
+                            d = d & "A"
+                        Case "5"
+                            d = d & "S"
+                        Case "6"
+                            d = d & "G"
+                        Case "7"
+                            d = d & "T"
+                        Case "8"
+                            d = d & "B"
+                        Case "9"
+                            d = d & "P"
+                        Case Else
+                            d = d & Mid(c, j, 1)
+                    End Select
+                Next
+
+                If EOF(1) <> True Then
+                    PrintLine(2, d)
+                    d = ""
+                Else
+                    Print(2, d)
+                    Exit Do
+                End If
+                c = LineInput(1)
+            Loop Until c = vbNewLine
+
+        Next
+
+    End Sub
+End Class
---
+++ Q12416/Q12416/Form1.vb
@@ -0,0 +1,40 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        FileOpen(1, "in1.txt", 1)
+        FileOpen(2, "out1.txt", 2)
+
+        Dim a, b As Long
+        Dim c As String
+
+        Do Until EOF(1) = True
+            c = LineInput(1)
+
+            For i = 1 To Len(c)
+                Select Case Mid(c, i, 1)
+                    Case " "
+                        a = a + 1
+                    Case Else
+                        If a > b Then
+                            b = a
+                        End If
+                        a = 0
+                End Select
+            Next
+            a = 0
+            Do Until b = 1
+                b = b \ 2 + b Mod 2
+                a = a + 1
+            Loop
+
+            If EOF(1) <> True Then
+                PrintLine(2, a)
+            Else
+                Print(2, a)
+            End If
+            a = 0
+            b = 0
+        Loop
+
+    End Sub
+End Class
