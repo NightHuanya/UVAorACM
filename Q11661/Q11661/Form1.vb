9055a6dbc3e733a4ed0ab9061527be2209ed4863
NightHuanya <47856051+NightHuanya@users.noreply.github.com>
2022-12-08 22:00:56 +0800

    Add files via upload

---
+++ Q11661/Q11661/Form1.vb
@@ -0,0 +1,70 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        FileOpen(1, "in1.txt", 1)
+        FileOpen(2, "out1.txt", 2)
+        Dim a, b, h As Long
+        Dim d$
+        Dim c As Boolean = False
+        Dim f As Boolean = False
+        Dim g As Boolean = False
+        h = 2000000
+        a = LineInput(1)
+        Do Until a = 0
+            d = LineInput(1)
+            For i = 1 To a
+                If Mid(d, i, 1) = "Z" Then
+                    h = 0
+                    Exit For
+                ElseIf Mid(d, i, 1) = "R" Then
+                    If c = False Then
+                        c = True
+                        b = 1
+                        f = True
+                    Else
+                        If g = False Then
+                            b = 1
+                        Else
+                            If b < h Then
+                                h = b
+                            End If
+                            g = False
+                            f = True
+                            b = 1
+                        End If
+                        End If
+                ElseIf Mid(d, i, 1) = "D" Then
+                        If c = False Then
+                            c = True
+                            b = 1
+                            g = True
+                        Else
+                            If f = False Then
+                            b = 1
+                        Else
+                            If b < h Then
+                                h = b
+                            End If
+                            f = False
+                            g = True
+                            b = 1
+                        End If
+                        End If
+                ElseIf Mid(d, i, 1) = "." Then
+                        If c = True Then b = b + 1
+                End If
+            Next
+            c = False
+            f = False
+            g = False
+            a = LineInput(1)
+            If a = 0 Then
+                Print(2, h)
+            Else
+                PrintLine(2, h)
+            End If
+            b = 0
+            h = 2000000
+        Loop
+    End Sub
+End Class
---
+++ Q12602/Q12602/Form1.vb
@@ -0,0 +1,50 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+        FileOpen(1, "in1.txt", 1)
+        FileOpen(2, "out1.txt", 2)
+
+        Dim a, b, c, d As String
+        Dim f As Integer
+
+        f = LineInput(1)
+
+        For i = 1 To f
+            a = LineInput(1)
+
+            For j = 1 To Len(a)
+                Select Case Mid(a, j, 1)
+                    Case "A" To "Z"
+                        b = Val(b) + (Asc(Mid(a, j, 1)) - 65) * 26 ^ (3 - j)
+                    Case "0" To "9"
+                        c = c & Mid(a, j, 1)
+                End Select
+            Next
+
+            If Math.Abs(Val(b) - Val(c)) <= 100 Then
+
+                If i = f Then
+                    Print(2, "nice")
+                Else
+                    PrintLine(2, "nice")
+                End If
+
+            Else
+
+                If i = f Then
+                    Print(2, "not nice")
+                Else
+                    PrintLine(2, "not nice")
+                End If
+
+            End If
+
+            b = ""
+            c = b
+            d = b
+
+        Next
+
+    End Sub
+End Class
